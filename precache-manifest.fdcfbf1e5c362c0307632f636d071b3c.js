660e9f4fc6cd89b05348ab900f21b6f61a9514e7
TonyS <tonysilvasd@gmail.com>
2019-07-16 18:53:18 -0700

    Updates

---
+++ precache-manifest.fdcfbf1e5c362c0307632f636d071b3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18f387f10652058dc0e0d9fb852b1a5",
+    "url": "/monsters-rolo/index.html"
+  },
+  {
+    "revision": "c8d382c7ab5dbe8258fc",
+    "url": "/monsters-rolo/static/css/main.a8dbbb8e.chunk.css"
+  },
+  {
+    "revision": "dff8f5e6896d4e47fb65",
+    "url": "/monsters-rolo/static/js/2.d16ba94f.chunk.js"
+  },
+  {
+    "revision": "c8d382c7ab5dbe8258fc",
+    "url": "/monsters-rolo/static/js/main.c4961828.chunk.js"
+  },
+  {
+    "revision": "a98d98c538772a665fc2",
+    "url": "/monsters-rolo/static/js/runtime~main.61995744.js"
+  }
+]);
